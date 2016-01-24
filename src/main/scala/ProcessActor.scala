a36dd03a2989d423c5520f20b4646e17caa73418
Dave Mills <dave.a.mills@gmail.com>
2016-01-24 00:33:52 -0500

    some work on interactive processes

--- src/main/scala/ProcessActor.scala
+++ src/main/scala/ProcessActor.scala
@@ -1,13 +1,16 @@
 import java.io.File
+import java.util.concurrent.TimeUnit
 
-import akka.actor.{ActorLogging, Actor}
+import akka.actor._
+import akka.util.Timeout
 import scala.concurrent.Future
 import scala.io.Source
-//import scala.sys.process.processInternal.OutputStream
+import java.io.OutputStream
+
 import scala.sys.process.{ProcessIO, ProcessLogger, Process}
-import scala.language.implicitConversions
 
 object ProcessActor {
+  import scala.language.implicitConversions
   implicit def string2seq(s:String):Seq[String] = s.split(' ')
   case class Request(workingDirectory:File, args:Seq[String])
   case class StdErr(s:String)
@@ -30,35 +33,82 @@ class ProcessActor extends Actor with ActorLogging {
   }
 }
 
-//object ProcessActor2 {
-//  implicit def string2seq(s:String):Seq[String] = s.split(' ')
-//  case class Request(workingDirectory:File, args:Seq[String])
-//  case class StdErr(s:String)
-//  case class StdOut(s:String)
-//  case class Finished(returnCode:Int)
-//}
-//
-//class ProcessActor2 extends Actor with ActorLogging {
-//  import ProcessActor2._
-//  import akka.pattern.pipe
-//  implicit val c = context.dispatcher
-//
-//  def working(stdin:OutputStream):Receive = { log.debug("state -> working"); {
-//    case _ => ???
-//  }}
-//
-//  def idle:Receive = { log.debug("state -> idle"); {
-//    case Request(workingDirectory, args) => log.debug(s"cwd=$workingDirectory args=$args")
-//      val b = Process(args, workingDirectory)
-//
-////      val l = new ProcessIO(
-////        stdin => stdin.context.become(working(stdin)),
-////        stdout => Source.fromInputStream(stdout).getLines().foreach(sender ! StdOut(_)),
-////        stderr => Source.fromInputStream(stderr).getLines().foreach(sender ! StdErr(_))
-////      )
-////
-////      Future { b.run(l).exitValue() }.mapTo[Int].map(Finished).pipeTo(sender())
-//  }}
-//
-//  def receive = idle
-//}
+object ProcessActor2 {
+  import scala.language.implicitConversions
+  implicit def string2seq(s:String):Seq[String] = s.split(' ')
+  case class Run(args:Seq[String], workingDirectory:Option[File]=None)
+  case class StdErr(s:String)
+  case class StdOut(s:String)
+  case class WriteLine(s:String)
+  case class Close()
+  case class Finished(returnCode:Int)
+}
+
+class ProcessActor2 extends Actor with ActorLogging {
+  import ProcessActor2._
+  import akka.pattern.pipe
+  implicit val c = context.dispatcher
+
+  def working(stdin:OutputStream):Receive = { log.debug("state -> working"); {
+    case WriteLine(s) => stdin.write(s"$s\n".getBytes)
+    case Close() => stdin.close()
+  }}
+
+  def idle:Receive = { log.debug("state -> idle"); {
+    case Run(args, workingDirectory) => log.debug(s"cwd=$workingDirectory args=$args")
+      val b = Process(args, workingDirectory)
+
+      val requester = sender()
+
+      val l = new ProcessIO(
+        stdin => context.become(working(stdin)),
+        stdout => Source.fromInputStream(stdout).getLines().foreach(requester ! StdOut(_)),
+        stderr => Source.fromInputStream(stderr).getLines().foreach(requester ! StdErr(_))
+      )
+
+      Future { b.run(l).exitValue() }
+        .map(Finished)
+        .pipeTo(sender())
+        .andThen { case _ => self ! PoisonPill }
+  }}
+
+  def receive = idle
+}
+
+object Process2Tester extends App {
+  import java.io.File
+  import java.util.concurrent.TimeUnit
+
+  import akka.actor._
+  import akka.util.Timeout
+  import scala.concurrent.Future
+  import scala.io.Source
+  import java.io.OutputStream
+
+  import scala.sys.process.{ProcessIO, ProcessLogger, Process}
+
+  import ProcessActor2._
+
+  implicit val system = ActorSystem()
+  implicit val timeout = new Timeout(10, TimeUnit.SECONDS)
+  import system.dispatcher
+
+  class Watcher extends Actor with ActorLogging {
+    def watching(p:ActorRef):Receive = {
+      case Terminated(johnConnor) =>
+        system.terminate()
+        sys.exit()
+      case a:Any => p ! a
+    }
+    def receive = {
+      case r:Run =>
+        val p = context.actorOf(Props[ProcessActor2])
+        context.watch(p)
+        p ! r
+        context.become(watching(p))
+    }
+  }
+  val w = system.actorOf(Props[Watcher])
+
+  w ! Run("heroku run rails c -a spaceman-staging")
+}
