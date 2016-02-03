0e7b739bce5cc9e9d2fbe05a99cd85ecfa293723
Dave Mills <dmills@wework.com>
2016-02-02 22:41:43 -0500

    refactor

--- src/main/scala/GithubActor.scala
+++ src/main/scala/GithubActor.scala
@@ -10,15 +10,13 @@ object GithubActor {
   case class CloneRepo(org:String, project:String)
   case class RepoCloned(repo:File)
   case class Checkout(branch:String)
-  case class CheckedOut()
   case class Merge(other:String)
-  case class Merged()
   case class AbortMerge()
-  case class MergeAborted()
   case class Push(remote:String)
-  case class Pushed()
   case class DeleteBranch(branch:String, remote:String)
-  case class BranchDeleted()
+  case class CheckCIStatus()
+  case class CIStatus(status:String)
+  case class Succeeded()
 }
 
 class GithubActor extends Actor with ActorLogging {
@@ -26,79 +24,63 @@ class GithubActor extends Actor with ActorLogging {
 
   val processor = context.actorOf(Props[ProcessActor])
 
-  def abortingMerge(org:String, project:String, repo:File, requester:ActorRef):Receive = {
+  def abortingMerge(requester:ActorRef):Receive = {
     case Finished(r:Int) if r == 0 =>
-      requester ! MergeAborted()
-      context.become(cloned(org, project, repo))
+      requester ! Succeeded()
+      context.unbecome() // mergeFailed
+      context.unbecome() // merging
+      context.unbecome() // ready
     case Finished(r:Int) if r != 0 =>
       requester ! Status.Failure(new Exception(s"merge abort failed: $r"))
-      context.become(mergeFailed(org, project, repo))
+      context.unbecome()
   }
 
-  def mergeFailed(org:String, project:String, repo:File):Receive = {
+  def mergeFailed(repo:File):Receive = {
     case AbortMerge =>
       processor ! Request(repo, "git merge --abort")
-      context.become(abortingMerge(org, project, repo, sender()))
+      context.become(abortingMerge(sender()), discardOld=false)
   }
 
-  def merging(org:String, project:String, repo:File, requester:ActorRef):Receive = {
+  def merging(repo:File, requester:ActorRef):Receive = {
     case Finished(r:Int) if r == 0 =>
-      requester ! Merged()
-      context.become(cloned(org, project, repo))
+      requester ! Succeeded()
+      context.unbecome()
     case Finished(r:Int) if r != 0 =>
       requester ! Status.Failure(new Exception(s"merge failed: $r"))
-      context.become(mergeFailed(org, project, repo))
+      context.become(mergeFailed(repo), discardOld=false)
   }
 
-  def checkingOut(org:String, project:String, repo:File, requester:ActorRef):Receive = {
+  def working(requester:ActorRef, failureMessage:String):Receive = {
     case Finished(r:Int) if r == 0 =>
-      requester ! CheckedOut()
-      context.become(cloned(org, project, repo))
+      requester ! Succeeded()
+      context.unbecome()
     case Finished(r:Int) if r != 0 =>
-      requester ! Status.Failure(new Exception(s"checkout failed: $r"))
-      context.become(cloned(org, project, repo))
+      requester ! Status.Failure(new Exception(s"$failureMessage: $r"))
+      context.unbecome()
   }
 
-  def pushing(org: String, project: String, repo: File, requester: ActorRef):Receive = {
-    case Finished(r:Int) if r == 0 =>
-      requester ! Pushed()
-      context.become(cloned(org, project, repo))
-    case Finished(r:Int) if r != 0 =>
-      requester ! Status.Failure(new Exception(s"push failed: $r"))
-      context.become(cloned(org, project, repo))
-  }
-
-  def deletingBranch(org: String, project: String, repo: File, requester:ActorRef):Receive = {
-    case Finished(r:Int) if r == 0 =>
-      requester ! BranchDeleted()
-      context.become(cloned(org, project, repo))
-    case Finished(r:Int) if r != 0 =>
-      requester ! Status.Failure(new Exception(s"delete failed: $r"))
-      context.become(cloned(org, project, repo))
-  }
-
-  def cloned(org:String, project:String, repo:File):Receive = {
+  def ready(org:String, project:String, repo:File):Receive = {
     case Checkout(branch) =>
       processor ! Request(repo, s"git checkout $branch")
-      context.become(checkingOut(org, project, repo, sender()))
+      context.become(working(sender(), "checking out failed"), discardOld=false)
     case Merge(other) =>
       processor ! Request(repo, s"git merge --no-ff origin/$other")
-      context.become(merging(org, project, repo, sender()))
+      context.become(merging(repo, sender()), discardOld=false)
     case Push(remote) =>
       processor ! Request(repo, s"git push $remote")
-      context.become(pushing(org, project, repo, sender()))
+      context.become(working(sender(), "pushing failed"), discardOld=false)
     case DeleteBranch(branch, remote) =>
       processor ! Request(repo, s"git push $remote --delete $branch")
-      context.become(deletingBranch(org, project, repo, sender()))
+      context.become(working(sender(), "deleting branch failed"), discardOld=false)
   }
 
   def cloning(org:String, project:String, requester:ActorRef, repo:File):Receive = {
     case Finished(r:Int) if r == 0 =>
       requester ! RepoCloned(repo)
-      context.become(cloned(org, project, repo))
+      context.become(ready(org, project, repo))
     case Finished(r:Int) if r != 0 =>
       requester ! Status.Failure(new Exception(s"clone failed: $r"))
-      context.become(empty)
+      context.unbecome()
   }
 
   def empty:Receive = {
@@ -106,7 +88,7 @@ class GithubActor extends Actor with ActorLogging {
       Try { Files.createTempDirectory("repo").toFile } match {
         case Success(parent) =>
           processor ! Request(parent, s"git clone git@github.com:$org/$project.git")
-          context.become(cloning(org, project, sender(), new File(parent, project)))
+          context.become(cloning(org, project, sender(), new File(parent, project)), discardOld = false)
         case Failure(ex) => sender() ! Status.Failure(ex)
       }
   }
