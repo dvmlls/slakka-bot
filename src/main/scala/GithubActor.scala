605d80a462d2eac090395b98aabb4c729bc63887
Dave Mills <dmills@wework.com>
2016-02-02 22:13:26 -0500

    wrapper actors for processes and github

---
+++ src/main/scala/GithubActor.scala
@@ -0,0 +1,102 @@
+import java.io.File
+import java.nio.file.Files
+
+import ProcessActor._
+import akka.actor._
+
+import scala.util.{Failure, Success, Try}
+
+object GithubActor {
+  case class CloneRepo(org:String, project:String)
+  case class RepoCloned(repo:File)
+  case class Checkout(branch:String)
+  case class CheckedOut()
+  case class Merge(other:String)
+  case class Merged()
+  case class AbortMerge()
+  case class MergeAborted()
+  case class Push(destination:String)
+  case class Pushed()
+}
+
+class GithubActor extends Actor with ActorLogging {
+  import GithubActor._
+
+  val processor = context.actorOf(Props[ProcessActor])
+
+
+  def abortingMerge(org:String, project:String, repo:File, requester:ActorRef):Receive = {
+    case Finished(r:Int) if r == 0 =>
+      requester ! MergeAborted()
+      context.become(cloned(org, project, repo))
+    case Finished(r:Int) if r != 0 =>
+      requester ! Status.Failure(new Exception(s"merge abort failed: $r"))
+      context.become(mergeFailed(org, project, repo))
+  }
+
+  def mergeFailed(org:String, project:String, repo:File):Receive = {
+    case AbortMerge =>
+      processor ! Request(repo, "git merge --abort")
+      context.become(abortingMerge(org, project, repo, sender()))
+  }
+
+  def merging(org:String, project:String, repo:File, requester:ActorRef):Receive = {
+    case Finished(r:Int) if r == 0 =>
+      requester ! Merged()
+      context.become(cloned(org, project, repo))
+    case Finished(r:Int) if r != 0 =>
+      requester ! Status.Failure(new Exception(s"merge failed: $r"))
+      context.become(mergeFailed(org, project, repo))
+  }
+
+  def checkingOut(org:String, project:String, repo:File, requester:ActorRef):Receive = {
+    case Finished(r:Int) if r == 0 =>
+      requester ! CheckedOut()
+      context.become(cloned(org, project, repo))
+    case Finished(r:Int) if r != 0 =>
+      requester ! Status.Failure(new Exception(s"checkout failed: $r"))
+      context.become(cloned(org, project, repo))
+  }
+
+  def pushing(org: String, project: String, repo: File, requester: ActorRef):Receive = {
+    case Finished(r:Int) if r == 0 =>
+      requester ! Pushed()
+      context.become(cloned(org, project, repo))
+    case Finished(r:Int) if r != 0 =>
+      requester ! Status.Failure(new Exception(s"push failed: $r"))
+      context.become(cloned(org, project, repo))
+  }
+
+  def cloned(org:String, project:String, repo:File):Receive = {
+    case Checkout(branch) =>
+      processor ! Request(repo, s"git checkout $branch")
+      context.become(checkingOut(org, project, repo, sender()))
+    case Merge(other) =>
+      processor ! Request(repo, s"git merge --no-ff origin/$other")
+      context.become(merging(org, project, repo, sender()))
+    case Push(destination) =>
+      processor ! Request(repo, s"git push $destination")
+      context.become(pushing(org, project, repo, sender()))
+  }
+
+  def cloning(org:String, project:String, requester:ActorRef, repo:File):Receive = {
+    case Finished(r:Int) if r == 0 =>
+      requester ! RepoCloned(repo)
+      context.become(cloned(org, project, repo))
+    case Finished(r:Int) if r != 0 =>
+      requester ! Status.Failure(new Exception(s"clone failed: $r"))
+      context.become(empty)
+  }
+
+  def empty:Receive = {
+    case CloneRepo(org, project) =>
+      Try { Files.createTempDirectory("repo").toFile } match {
+        case Success(parent) =>
+          processor ! Request(parent, s"git clone git@github.com:$org/$project.git")
+          context.become(cloning(org, project, sender(), new File(parent, project)))
+        case Failure(ex) => sender() ! Status.Failure(ex)
+      }
+  }
+
+  def receive = empty
+}
---
+++ src/main/scala/ProcessActor.scala
@@ -0,0 +1,27 @@
+import java.io.File
+
+import akka.actor.{ActorLogging, Actor}
+import scala.sys.process.ProcessLogger
+import scala.sys.process.Process
+import scala.language.implicitConversions
+
+object ProcessActor {
+  implicit def string2seq(s:String):Seq[String] = s.split(' ')
+  case class Request(workingDirectory:File, args:Seq[String])
+  case class StdErr(s:String)
+  case class StdOut(s:String)
+  case class Finished(returnCode:Int)
+}
+
+class ProcessActor extends Actor with ActorLogging {
+  import ProcessActor._
+  implicit val c = context.dispatcher
+
+  def receive = {
+    case Request(workingDirectory, args) =>
+      val l = ProcessLogger(sender ! StdOut(_), sender ! StdErr(_))
+      val p = Process(args, workingDirectory)
+
+      sender() ! Finished(p ! l)
+  }
+}
