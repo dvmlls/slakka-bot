c5c59daa02de4685b6490a8693ecc521d59d6cfe
Dave Mills <dmills@wework.com>
2016-02-05 18:07:06 -0500

    5x: 'retest this please', wait 30 seconds, then retry

--- src/main/scala/Autobot.scala
+++ src/main/scala/Autobot.scala
@@ -7,12 +7,31 @@ import scala.concurrent.{ExecutionContext, Future}
 object Autobot {
   def autoMerge(org:String, proj:String, pr:Int, poll:String => Future[CIStatus])
                (implicit sys:ActorSystem, ctx:ExecutionContext) = {
+
+    val pollForFailures:String => Future[CIStatus] = (sha:String) => poll(sha).flatMap {
+      case f:CIFailure => Future.failed(new Exception(s"ci failed - retry?"))
+      case s:CIStatus => Future { s }
+    }
+
+    def retry(sha:String) = for (
+      _ <- comment(org, proj, pr, "retest this please");
+      _ <- Future { Thread.sleep(30 * 1000) };
+      s <- pollForFailures(sha)
+    ) yield s
+
+    val retryPoll = (sha:String) => pollForFailures(sha)
+      .recoverWith { case _ =>  retry(sha) }
+      .recoverWith { case _ =>  retry(sha) }
+      .recoverWith { case _ =>  retry(sha) }
+      .recoverWith { case _ =>  retry(sha) }
+      .recoverWith { case _ =>  retry(sha) }
+
     for (
       (branchName, sha) <- getPR(org, proj, pr).flatMap {
         case PR(_, "open", PRHead(branchName, sha), _, _) => Future { (branchName, sha) }
         case PR(_, failed, _, _, _) => Future.failed(new Exception(s"pr not open: $failed"))
       };
-      _ <- poll(sha).flatMap {
+      _ <- retryPoll(sha).flatMap {
         case s:CISuccess => Future { true }
         case a:Any => Future.failed(new Exception(s"polling for CI status failed: $a"))
       };
